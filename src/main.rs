2b0a3d3c7f081fad03a033f1cd89600e27182548
Elias Cox <eliascox555@gmail.com>
2024-01-17 11:46:02 +1100

    created instance and device queues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use vulkano::VulkanLibrary;
+use vulkano::instance::{Instance, InstanceCreateInfo};
+
 fn main() {
-    println!("Hello, world!");
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default())
+    .expect("failed to create instance");
+
+    //Note this picks the first device, not the best atm
+    let physical_device = instance
+    .enumerate_physical_devices()
+    .expect("could not enumerate devices")
+    .next()
+    .expect("no devices available");
+
+    for family in physical_device.queue_family_properties() {
+        println!("Found a queue family with {:?} queue(s)", family.queue_count);
+    }
+
 }
+
+
