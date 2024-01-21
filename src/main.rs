acf3bc65cccc26a00b9266a2babc7a134a660150
Elias Cox <eliascox555@gmail.com>
2024-01-21 12:37:14 +1100

    device

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use vulkano::VulkanLibrary;
 use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::device::QueueFlags;
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo};
 
 fn main() {
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
@@ -17,6 +19,28 @@ fn main() {
         println!("Found a queue family with {:?} queue(s)", family.queue_count);
     }
 
+    let queue_family_index = physical_device
+    .queue_family_properties()
+    .iter()
+    .enumerate()
+    .position(|(_queue_family_index, queue_family_properties)| {
+        queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
+    })
+    .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            // here we pass the desired queue family to use by index
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+    
 }
 
 
